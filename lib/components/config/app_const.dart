fceb2040fd5ffab4de779b6513a6d6beb99ce640
Yudi Herlambang <yudiherlambang09@gmail.com>
2025-01-06 12:51:29 +0700

    Percobaan parsing

--- lib/components/config/app_const.dart
+++ lib/components/config/app_const.dart
@@ -1,7 +1,8 @@
 class AppConst {
   static String appName = "based-project";
   static bool isDebuggable = true;
-  static String appUrl = "https://www.themealdb.com/api/json/v1/1/";
+  static String appUrl =
+      "https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League";
   static String imageExample =
       "https://images.ctfassets.net/47xts72n4555/2EkPAUYLoJWIZtFOJpM3ng/af3682ce5e7a14121a9517e38321026b/Skye-thumbnail.png";
 }
